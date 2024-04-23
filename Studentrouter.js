d4c1ae56a7d04ab0377834202f188a90d9527e6f
shalini <s.rathore43@gmail.com>
2024-04-23 10:37:45 +0530

    routing demo commit

---
+++ Studentrouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+router.get("/",(req,res)=>
+{
+    res.send("st udent home ")
+})
+router.get("/insertstudent",(req,res)=>
+{
+    res.send("insert student section");
+})
+router.get("/updatestudent",(req,res)=>
+{
+    res.send("update student section");
+})
+router.get("/deletestudent",(req,res)=>
+{
+    res.send("delete student section");
+})
+// exports.default =  router;
+module.exports =router;
